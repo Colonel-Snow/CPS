be5ffc1abffd939a5ac8f82c67450e371c20baf6
Erin <eetolman@hotmail.com>
2019-03-27 12:44:13 -0800

    add setters and getters, corrections to constructors

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 //
 // Created by kerne on 3/22/2019.
 //
-
+#include <iostream>
+#include "polygon.h"
 int main(){
-
-
+    Polygon hexagon(6, 1.0);
+    std::cout << hexagon.getHeight() << std::endl;
+    return 0;
 };
